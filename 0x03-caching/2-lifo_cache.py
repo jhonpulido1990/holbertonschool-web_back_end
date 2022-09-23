6518e1dcac693452c7c72ff747808ae046c5a5cb
jhonpulido1990 <3825@holbertonstudents.com>
2022-09-23 13:15:23 -0500

    add file

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -10,24 +10,21 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         ''' Initialize class instance. '''
         super().__init__()
-        self.current_keys = []
+        self.last_key = ''
 
     def put(self, key, item):
         """ Add an item in the cache """
-        if key is not None or item is not None:
+        if key and item:
             self.cache_data[key] = item
-            if key not in self.current_keys:
-                self.current_keys.append(key)
-            else:
-                self.current_keys.append(self.current_keys.pop(
-                    self.current_keys.index(key)))
-            if len(self.current_keys) > BaseCaching.MAX_ITEMS:
-                discarded_key = self.current_keys.pop(-2)
-                del self.cache_data[discarded_key]
-                print('DISCARD: {}'.format(discarded_key))
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                print("DISCARD: {}".format(self.last_key))
+                self.cache_data.pop(self.last_key)
+            self.last_key = key
 
     def get(self, key):
-        """ Get an item by key """
+        """ Return the value linked """
         if key is None or self.cache_data.get(key) is None:
             return None
-        return self.cache_data.get(key)
+        if key in self.cache_data:
+            value = self.cache_data[key]
+            return value
