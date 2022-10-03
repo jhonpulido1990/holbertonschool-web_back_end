30323edda1704b6e9ae3de7bd3dcb18c8cc60d73
jhonpulido1990 <3825@holbertonstudents.com>
2022-10-03 15:22:07 -0500

    task 4

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
-
+''' Basic authentication '''
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth():
+    ''' a class to manage the API authentication '''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        return False
+        ''' Define which routes don't need authentication '''
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths or (path + '/') in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         return None
