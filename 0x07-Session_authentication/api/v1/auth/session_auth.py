5684324953f88f662ac53d28c327ed142e6dcd6b
jhonpulido1990 <3825@holbertonstudents.com>
2022-10-05 15:04:30 -0500

    1. Empty session

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+API session authentication module
+"""
+
+from api.v1.auth.auth import Auth
+from models.user import User
+import uuid
+
+
+class SessionAuth(Auth):
+    """ Session Authentication """
