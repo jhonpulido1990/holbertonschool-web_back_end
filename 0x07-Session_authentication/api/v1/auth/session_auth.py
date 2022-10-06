e39d77fb7832d50e4e846e688eb1eadb65ef98b5
jhonpulido1990 <3825@holbertonstudents.com>
2022-10-06 11:17:54 -0500

    change == to is

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -17,9 +17,9 @@ class SessionAuth(Auth):
         '''The same user_id can have multiple Session ID -
         indeed, the user_id is the value in the dictionary
         user_id_by_session_id'''
-        if user_id == None:
+        if user_id is None:
             return None
-        if isinstance(user_id, str) == False:
+        if isinstance(user_id, str) is False:
             return None
         seccion_id = str(uuid.uuid4())
         self.user_id_by_session_id[seccion_id] = user_id
