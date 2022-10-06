235bb98d2efc275c53bbf62805aa98f89dc2d0fd
jhonpulido1990 <3825@holbertonstudents.com>
2022-10-06 15:00:43 -0500

    add task 5

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -59,7 +59,7 @@ def request_filter() -> None:
 
     if auth:
         if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None and\
+            if auth.authorization_header(request) is None or\
                 auth.session_cookie(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
