2cb3ce3031401d9c6a5af7a703b94a7a7cf5435a
jhonpulido1990 <3825@holbertonstudents.com>
2022-10-10 16:23:42 -0500

    add comment

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
-""" User model """
+"""In this task you will create a SQLAlchemy model named User
+for a database table named users (by using the mapping declaration
+of SQLAlchemy).
+"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 
 class User(Base):
-    """ SQLAlchemy model for a database table named users """
+    """The model will have the following attributes:
+    id, the integer primary key
+    email, a non-nullable string
+    hashed_password, a non-nullable string
+    session_id, a nullable string
+    reset_token, a nullable string"""
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
