009f393033df4a7b162d094ff5e6a7f66e73641e
jhonpulido1990 <3825@holbertonstudents.com>
2022-10-13 10:27:12 -0500

    change elif

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -18,6 +18,7 @@ def index() -> str:
     """
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users() -> str:
     """ POST /users
@@ -32,7 +33,7 @@ def users() -> str:
 
     if "email" not in form_data:
         return jsonify({"message": "email required"}), 400
-    elif "password" not in form_data:
+    if "password" not in form_data:
         return jsonify({"message": "password required"}), 400
     else:
 
@@ -48,6 +49,7 @@ def users() -> str:
         except ValueError:
             return jsonify({"message": "email already registered"}), 400
 
+
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login() -> str:
     """ POST /sessions
@@ -60,7 +62,7 @@ def login() -> str:
 
     if "email" not in form_data:
         return jsonify({"message": "email required"}), 400
-    elif "password" not in form_data:
+    if "password" not in form_data:
         return jsonify({"message": "password required"}), 400
     else:
 
@@ -79,6 +81,7 @@ def login() -> str:
 
             return response
 
+
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout() -> None:
     """ DELETE /sessions
@@ -94,6 +97,7 @@ def logout() -> None:
             return redirect(url_for('index'))
     else:
         abort(403)
+
 
 @app.route('/profile', methods=['GET'], strict_slashes=False)
 def profile() -> str:
@@ -114,6 +118,7 @@ def profile() -> str:
             abort(403)
     else:
         abort(403)
+
 
 @app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token() -> str:
@@ -139,6 +144,7 @@ def get_reset_password_token() -> str:
             }), 200
         except ValueError:
             abort(403)
+
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password() -> str:
