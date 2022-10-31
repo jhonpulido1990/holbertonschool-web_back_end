5e43fa70f94e41be032eb9f3cf372f02ac2e3d63
jhonpulido1990 <3825@holbertonstudents.com>
2022-10-31 11:30:29 -0500

    add funtion store in task 0

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,12 +2,22 @@
 """
 reddis
 """
+from typing import Union, Callable, Optional, Any
 import redis
+import uuid
 
 
 class Cache:
+    """ class """
     def __init__(self):
         """ constructor - store an instance of the Redis client as a private
         variable named _redis and flush the instance using flushdb """
         self._redis = redis.Redis()
         self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ generate a random key (e.g. using uuid), store the input data in
+        Redis using the random key and return the key """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
