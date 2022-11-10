45ce1fd951646ecaaf35340e3fce5972cdbf5815
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-10 00:23:17 -0500

    For...of Loops

---
+++ 0x0E-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    let new_array = []
+    for (let idx of array) {
+        new_array.push(appendString + idx);
+    }
+
+    return new_array;
+}
