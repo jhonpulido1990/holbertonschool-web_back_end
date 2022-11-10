8f1f3ab2bc83d6a6c02add7c1f6d0a0abf43c142
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-10 15:43:23 -0500

    add spaces 2 in tabulation

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    let new_array = []
-    for (let idx of array) {
-        new_array.push(appendString + idx);
-    }
+  let new_array = []
+  for (let idx of array) {
+    new_array.push(appendString + idx);
+  }
 
-    return new_array;
+  return new_array;
 }
