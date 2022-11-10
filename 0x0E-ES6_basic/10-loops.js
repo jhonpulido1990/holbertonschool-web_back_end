e87607849ea13da355881b86faa126e51cb03869
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-10 15:45:31 -0500

    add semicolom in for

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -2,7 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   let new_array = []
   for (let idx of array) {
     new_array.push(appendString + idx);
-  }
+  };
 
   return new_array;
 }
