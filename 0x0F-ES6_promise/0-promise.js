502e69d7a08750ab953c1f0d036cc8928ef9a549
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-14 11:13:17 -0500

    task 0

---
+++ 0x0F-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+function getResponseFromAPI() {
+  return Promise.resolve();
+}
+
+export default getResponseFromAPI;
+
+/*
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => resolve);
+}
+*/
