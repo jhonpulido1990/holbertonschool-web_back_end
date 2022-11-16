ab88579beb0952e1817207d338e906c1017a9201
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-16 17:12:07 -0500

    task 7

---
+++ 0x10-ES6_classes/7-airport.js
@@ -0,0 +1,11 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    const code = this._code;
+    return (`[object ${code}]`);
+  }
+}
