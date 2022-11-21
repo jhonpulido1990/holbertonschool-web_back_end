52459fe0b1c3f70c1bd79a87645af0c50ef57eec
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-21 16:33:58 -0500

    correction task 3

--- 0x11-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x11-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
 export default function getStudentIdsSum(arg) {
-  const array1 = arg.map(({ id }) => id);
-  const sum = 0;
-  return array1.reduce((accumulator, currentValue) => accumulator + currentValue, sum);
+  return arg.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
 }
