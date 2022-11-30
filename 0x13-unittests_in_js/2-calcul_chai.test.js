a348d1efd0ace63788f845c6104393a7a375deb7
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-30 17:24:01 -0500

    all change code

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -1,17 +1,22 @@
-const expect = require('chai').expect
+const { expect } = require('chai');
 const calculateNumber = require('./2-calcul_chai');
 
-describe('calculateNumber', function() {
-  it('Checks equality', function(){
-    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
-  });
-  it('Checks equality', function(){
-    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(4);
-  });
-  it('Checks equality', function(){
-    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
-  });
-  it('Checks equality', function(){
-    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
-  });
+describe('calculateNumber type === SUM', () => {
+	it('Addition', () => {
+		expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+	});
+});
+
+describe('calculateNumber type === SUBTRACT', () => {
+	it('Subtraction', () => {
+		expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+	});
+});
+
+describe('calculateNumber type === DIVIDE', () => {
+	it('Divide', () => {
+		expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+		expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+		expect(calculateNumber('DIVIDE', 0, 2)).to.equal(0);
+	});
 });
