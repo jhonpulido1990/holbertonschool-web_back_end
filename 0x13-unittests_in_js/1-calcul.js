e35fc6cdaf0c86b2785581618ea557b5ebb3fe0f
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-30 16:30:47 -0500

    change all code

--- 0x13-unittests_in_js/1-calcul.js
+++ 0x13-unittests_in_js/1-calcul.js
@@ -1,14 +1,18 @@
-function calculateNumber ( type, a, b) {
-  if (type === 'SUM') {
-    return Math.round(a) + Math.round(b);
-  }
-  if (type === 'SUBTRACT') {
-    return Math.round(b) - Math.round(a);
-  }
-  if (type === 'DIVIDE') {
-    if (b === 0) return 'Error';
-    return Math.round(a) / Math.round(b);
-  }
-}
+function calculateNumber(type, a, b) {
+  const roundA = Math.round(a);
+  const roundB = Math.round(b);
 
+  if (type === 'DIVIDE') {
+    if (roundB === 0) {
+      return 'Error';
+    }
+    return roundA / roundB;
+  }
+
+  if (type === 'SUBTRACT') {
+    return roundA - roundB;
+  }
+
+  return roundA + roundB;
+}
 module.exports = calculateNumber;
