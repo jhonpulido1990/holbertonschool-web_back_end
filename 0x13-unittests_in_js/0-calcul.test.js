7d759776865144702f3d29c3a9de68700e535212
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-30 15:26:21 -0500

    change title chech

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -2,22 +2,22 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function() {
-  it('Checks equality', function() {
+  it('...', function() {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
-  it('Checks equality', function() {
+  it('...', function() {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
-  it('Checks equality', function() {
+  it('...', function() {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
-  it('Checks equality', function() {
+  it('...', function() {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
-  it('Checks equality', function() {
+  it('...', function() {
     assert.strictEqual(calculateNumber(1.4, 4.5), 6);
   });
-  it('Checks equality', function() {
+  it('...', function() {
     assert.strictEqual(calculateNumber(0.5, 3.7), 5);
   });
 });
---
+++ 0x13-unittests_in_js/3-payment.js
@@ -0,0 +1,6 @@
+const utils = require('./utils')
+
+module.exports = function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  console.log(`The total is: ${utils.calculateNumber('SUM', totalAmount, totalShipping)}`);
+  return utils.calculateNumber('SUM', totalAmount, totalShipping);
+}
