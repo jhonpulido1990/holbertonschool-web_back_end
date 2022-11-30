ea19ef3d9ccd1bd169a6a47608dd8b09dc7d85ec
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-30 17:22:11 -0500

    change path file

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -1,12 +1,12 @@
 const expect = require('chai').expect
-const calculateNumber = require('./2-calcul');
+const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', function() {
   it('Checks equality', function(){
     expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
   });
   it('Checks equality', function(){
-    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(4);
   });
   it('Checks equality', function(){
     expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
