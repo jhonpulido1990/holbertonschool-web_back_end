ad6c1527cf12a0368847a17536f5d19973587ae3
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-30 15:21:14 -0500

    add fixed test

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -6,12 +6,18 @@ describe('calculateNumber', function() {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
   it('Checks equality', function() {
-    assert.strictEqual(calculateNumber(1, 3,7), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
   it('Checks equality', function() {
-    assert.strictEqual(calculateNumber(1.2, 3,7), 4);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
   it('Checks equality', function() {
-    assert.strictEqual(calculateNumber(1.5, 3,7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+  it('Checks equality', function() {
+    assert.strictEqual(calculateNumber(1.4, 4.5), 6);
+  });
+  it('Checks equality', function() {
+    assert.strictEqual(calculateNumber(0.5, 3.7), 5);
   });
 });
