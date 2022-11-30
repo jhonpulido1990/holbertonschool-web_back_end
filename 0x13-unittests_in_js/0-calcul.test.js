85dba70675dfd89dc3c4af68b8cf1b69b0bc0583
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-30 15:29:56 -0500

    fixed task 0

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,23 +1,14 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
-  it('...', function() {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-  it('...', function() {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-  it('...', function() {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-  it('...', function() {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
-  it('...', function() {
-    assert.strictEqual(calculateNumber(1.4, 4.5), 6);
-  });
-  it('...', function() {
-    assert.strictEqual(calculateNumber(0.5, 3.7), 5);
-  });
+describe('calculateNumber', () => {
+	it('...', () => {
+		assert.strictEqual(calculateNumber(1, 3), 4);
+		assert.strictEqual(calculateNumber(1, 3.7), 5);
+		assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+		assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+		assert.strictEqual(calculateNumber(-1.5, 3.5), 3);
+		assert.strictEqual(calculateNumber(1.5, -3.4), -1);
+		assert.strictEqual(calculateNumber(-1.2, -3.7), -5);
+	});
 });
