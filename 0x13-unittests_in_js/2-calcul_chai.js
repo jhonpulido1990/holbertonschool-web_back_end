620539527886b0882557cdd87d26e1c3e5ca96ff
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-30 17:15:36 -0500

    delete funtion word reserv

--- 0x13-unittests_in_js/2-calcul_chai.js
+++ 0x13-unittests_in_js/2-calcul_chai.js
@@ -1,4 +1,4 @@
-module.exports = function calculateNumber ( type, a, b) {
+module.exports = calculateNumber = ( type, a, b) => {
   if (type === 'SUM') {
     return Math.round(a) + Math.round(b);
   }
