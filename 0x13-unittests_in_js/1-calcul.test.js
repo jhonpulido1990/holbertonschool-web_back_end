5da57afd8c1d5372cccda0fd4f892883764e95b3
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-30 15:57:43 -0500

    fixed js

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -1,17 +1,26 @@
 const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
-describe('calculateNumber', function() {
-  it('Checks equality', function(){
-    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6)
-  });
-  it('Checks equality', function(){
+describe('calculateNumber type === SUM', () => {
+	it('Addition', () => {
+		assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+	});
+});
+
+describe('calculateNumber type === SUBTRACT', () => {
+  it('Subtraction', () => {
     assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), 4)
   });
-  it('Checks equality', function(){
+});
+
+describe('calculateNumber type === DIVIDE', () => {
+  it('Divide', () => {
     assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2)
-  });
-  it('Checks equality', function(){
+  })
+});
+
+describe('calculateNumber', function() {
+  it('Divide', function(){
     assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error')
   });
 });
