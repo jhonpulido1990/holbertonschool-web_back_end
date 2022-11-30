be2d6f4004cabf15d1c37aa461b7e562deae90d7
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-30 17:18:14 -0500

    correction resolve

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -6,7 +6,7 @@ describe('calculateNumber', function() {
     expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
   });
   it('Checks equality', function(){
-    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(4);
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
   });
   it('Checks equality', function(){
     expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
