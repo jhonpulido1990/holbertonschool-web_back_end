9b10df3b09a381ba3c7d1411cefc05edba074f06
jhonpulido1990 <3825@holbertonstudents.com>
2022-12-01 10:32:18 -0500

    fixed test

--- 0x13-unittests_in_js/6-payment_token.test.js
+++ 0x13-unittests_in_js/6-payment_token.test.js
@@ -1,11 +1,16 @@
 const { expect } = require('chai');
+
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
-describe('getPaymentTokenFromApi', () => {
-	it('Async function', (done) => {
-		getPaymentTokenFromAPI(true).then(() => {
-			expect(getPaymentTokenFromAPI.calledWith(true)).to.be.true;
-		});
-		done();
-	});
+describe('getPaymentTokenFromAPI', () => {
+  it('promise response from API should resolve', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((result) => {
+        expect(result).to.eql({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      });
+  });
 });
